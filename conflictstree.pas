cbbc94da94fe8a3f2b2b937c1cd2afff333b03dc
Иван <ivan.rusia@mail.ru>
2014-07-03 15:09:40 +1100

    Дерево конфликтов: доработка
    Добавить возможность по двойному щелчку мыши в дереве отобразить форму с
    соответствующими конфликтами

--- conflictstree.pas
+++ conflictstree.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, sqldb, FileUtil, Forms, Controls, Graphics, Dialogs,
-  ComCtrls, MetaUnit, ConflictsMeta;
+  ComCtrls, MetaUnit, ConflictsMeta, Filters, Referen;
 
 type
 
@@ -15,6 +15,7 @@ type
   TConflictsTreeForm = class(TForm)
     TreeView: TTreeView;
     procedure FormShow(Sender: TObject);
+    procedure TreeViewDblClick(Sender: TObject);
   public
     ScheduleTable: TTableInfo;
     procedure BuildTree;
@@ -32,11 +33,14 @@ procedure TConflictsTreeForm.InsertConflicts(ANode: TTreeNode; AID:  Integer);
 var
   i: Integer;
   Pairs: TConflictPairs;
+  tmpNode: TTreeNode;
 begin
   Pairs := ConflictsModule.ConflictPairs[AID];
-  for i := 0 to High(Pairs) do
-    TreeView.Items.AddChild(ANode, Format('Конфликт между %d и %d',
+  for i := 0 to High(Pairs) do begin
+    tmpNode := TreeView.Items.AddChild(ANode, Format('Конфликт между %d и %d',
       [Pairs[i].x, Pairs[i].y]));
+    tmpNode.Data := @ConflictsModule.ConflictPairs[AID][i];
+  end;
 end;
 
 procedure TConflictsTreeForm.BuildTree;
@@ -56,6 +60,22 @@ end;
 procedure TConflictsTreeForm.FormShow(Sender: TObject);
 begin
   BuildTree;
+end;
+
+procedure TConflictsTreeForm.TreeViewDblClick(Sender: TObject);
+var
+  a, b: String;
+  DefFilters: array of TFilterInfo;
+begin
+  if TreeView.Selected.Data = nil then Exit;
+  with TPoint(TreeView.Selected.Data^) do begin
+    a := IntToStr(x);
+    b := IntToStr(y);
+  end;
+  SetLength(DefFilters, 2);
+  DefFilters[0] := TFilterInfo.Create(0, 2, a, True);
+  DefFilters[1] := TFilterInfo.Create(0, 2, b, True);
+  ReferenForm.PopupForm(ConflictsModule.ScheduleTable, 1, DefFilters);
 end;
 
 {$R *.lfm}
